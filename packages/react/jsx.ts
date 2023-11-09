e361dfbb9fdaa16411d04c4514eb9a75035bbd0d
Finn <Finn-FY.Fu@aia.com>
2023-11-09 16:04:01 +0800

    feat: create ReactElement and JSX function and export createElement from react package

--- packages/react/jsx.ts
+++ packages/react/jsx.ts
@@ -1,12 +1,12 @@
 import { REACT_ELEMENT_TYPE } from "shared/ReactSymbols";
-import { Type, Key, Ref, Props, ReactElement } from "shared/ReactTypes";
+import { Type, Key, Ref, Props, ReactElementType } from "shared/ReactTypes";
 // ReactElement 数组结构
-export const reactElement = function (
+export const ReactElement = function (
   type: Type,
   key: Key,
   ref: Ref,
   props: Props,
-): ReactElement {
+): ReactElementType {
   const element = {
     $$typeof: REACT_ELEMENT_TYPE,
     type,
@@ -17,3 +17,48 @@ export const reactElement = function (
   };
   return element;
 };
+
+export const jsx = function (
+  type: ReactElementType,
+  config: any,
+  ...maybeChildren: any
+) {
+  // 单独处理 key 和 ref
+  let key: Key = null;
+  let ref: Ref = null;
+  const props: Props = {};
+
+  // 遍历config 给 key ref props赋值
+  for (const propNmae in config) {
+    const val = config[propNmae];
+    if (propNmae === "key") {
+      if (val != undefined) {
+        key = val + "";
+      }
+      continue;
+    }
+    if (propNmae === "ref") {
+      if (val != undefined) {
+        ref = val;
+      }
+      continue;
+    }
+    // 只取config自身的值
+    if (Object.hasOwnProperty.call(config, propNmae)) {
+      props[propNmae] = val;
+    }
+    // 根据maybeChildren 给props赋值
+    if (maybeChildren) {
+      if (maybeChildren.length === 1) {
+        props.children = maybeChildren[0];
+      } else {
+        props.children = maybeChildren;
+      }
+    }
+
+    // 返回ReactElement元素
+    return ReactElement(type, key, ref, props);
+  }
+};
+
+export const jsxDev = jsx;
